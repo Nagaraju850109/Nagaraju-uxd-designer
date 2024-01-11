d7aa1256fc33bc0988aa0c00dc3ce1f2b44d6a7b
geeta-patel-2 <gpatel2@student.touro.edu>
2024-01-10 22:10:20 -0500

    Changes for the github sites.

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,11 +1,12 @@
 import {Component, inject, TemplateRef} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {NgbOffcanvas} from "@ng-bootstrap/ng-bootstrap";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-top-bar',
     standalone: true,
-    imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage, RouterLink],
     templateUrl: './top-bar.component.html',
     styleUrl: './top-bar.component.scss'
 })
